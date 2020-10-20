07136b7368dc91fe5c10c13b2fef67b6519efa06
Daniel Sanchez <daniels@wolfram.com>
2020-10-19 20:41:39 -0500

    Adding HypergraphToGraph with Method -> "DistanceMatrix".

--- Kernel/HypergraphToGraph.m
+++ Kernel/HypergraphToGraph.m
@@ -6,6 +6,7 @@ PackageExport["HypergraphToGraph"]
 
 (* Documentation *)
 HypergraphToGraph::usage = usageString[
+<<<<<<< HEAD
   "HypergraphToGraph[`hg`, `method`] uses `method` to convert a hypergraph `hg` to a Graph."];
 
 (* Options *)
@@ -98,12 +99,70 @@ hypergraphToGraph[_, hgraph_ ? hypergraphQ, "StructurePreserving", opts : Option
           style[$lightTheme][$structurePreservingHyperedgeToHyperedgeEdgeStyle]]}],
       opts]
   ]
+=======
+  "HypergraphToGraph[`hg`] convert a hypergraph `hg` to a graph.",
+  "\n",
+  "HypergraphToGraph[`hg`, Method -> `method`] uses `method` for the conversion."];
+
+(* Options *)
+Options[HypergraphToGraph] = Join[{Method -> Automatic}, Options @ Graph];
+
+(* SyntaxInformation *)
+SyntaxInformation[HypergraphToGraph] = {
+  "ArgumentsPattern" -> {_, OptionsPattern[]},
+  "OptionNames" -> Options[HypergraphToGraph][All, 1]};
+
+(* Argument count *)
+HypergraphToGraph[args___] := 0 /;
+  !Developer`CheckArgumentCount[HypergraphToGraph[args], 1, 1] && False
+
+(* main *)
+expr : HypergraphToGraph[hgraph_, opts : OptionsPattern[HypergraphToGraph]] := ModuleScope[
+  res = Catch[hypergraphToGraph[HoldForm @ expr, hgraph, opts]];
+  res /; res =!= $Failed
+]
+
+(* methods *)
+$automaticMethod = "DistanceMatrix";
+$validMethods = {"DistanceMatrix", "Structure"};
+
+(* error messages *)
+HypergraphToGraph::bdmtd = "Value of option Method -> `1` is not" <>
+  StringJoin[Flatten @ Replace[ToString[#, InputForm] & /@, {a___, b_} :> {Riffle[{a}, ", "], " or ", b}]] <> ".";
+
+(* internal *)
+hypergraphToGraph[_, hgraph_ ? hypergraphQ, opts : OptionsPattern[]] := ModuleScope[
+  method = OptionValue[Method];
+  If[method =!= Automatic && !MemberQ[$validMethods, method],
+    Message[hypergraphToGraph::bdmtd, method];
+    Throw[$Failed],
+    method = Replace[method, Automatic -> $automaticMethod]
+  ];
+]
+
+(** Distance matrix preserving **)
+toGraph["DistanceMatrix", hgraph_, opts : OptionsPattern[]] :=
+  Graph[
+    Flatten[
+      Table[
+        DirectedEdge[edge[[j]], edge[[i]]],
+        {edge, hgraph},
+        {i, Length @ hgraph},
+        {j, i - 1}],
+      2],
+    FilterRules[{opts}, Graph]]
+
+(** Structure preserving **)
+>>>>>>> Adding HypergraphToGraph with Method -> "DistanceMatrix".
 
 (* Incorrect arguments messages *)
 hypergraphToGraph[expr_, hgraph_ ? (Not @* hypergraphQ), ___] :=
   (Message[HypergraphToGraph::invalidHypergraph, 1, HoldForm @ expr];
   Throw[$Failed])
+<<<<<<< HEAD
 
 hypergraphToGraph[expr_, _, method_, ___] /; !MemberQ[$validMethods, method] :=
   (Message[HypergraphToGraph::invalidMethod, HoldForm @ expr];
   Throw[$Failed])
+=======
+>>>>>>> Adding HypergraphToGraph with Method -> "DistanceMatrix".
